987cb2403dc412d48872ab78886bb5ea12ac9de9
furu <ba09219@gmail.com>
2014-08-11 13:52:38 +0900

    berks cookbook gitbucket-cookbook

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'gitbucket-cookbook'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures gitbucket-cookbook'
+long_description 'Installs/Configures gitbucket-cookbook'
+version          '0.1.0'
+
